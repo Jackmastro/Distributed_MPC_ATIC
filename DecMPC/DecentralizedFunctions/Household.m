c8f7045eb214c09e4e410e5ae7585c4774391bfb
mmutto <132058607+mmutto@users.noreply.github.com>
2024-06-16 14:42:04 +0200

    Decentralized
    CostFunction, Household and Dynamic modified

--- DecMPC/DecentralizedFunctions/Household.m
+++ DecMPC/DecentralizedFunctions/Household.m
@@ -4,9 +4,6 @@ classdef Household
     properties (Constant)
         rho_w = 1;
         cp_w  = 1;
-        V_F   = 1;
-        h_F   = 1;
-        A_F   = 1;
         V_S1  = 1;
         h_S1  = 1;
         A_S1  = 1;
@@ -19,12 +16,6 @@ classdef Household
         V_S3  = 1;
         h_S3  = 1;
         A_S3  = 1;
-        V_R   = 1;
-        h_R   = 1;
-        A_R   = 1;
-        V_B   = 1;
-        h_B   = 1;
-        A_B   = 1;
     end
     
     % Public household properties 
@@ -39,50 +30,24 @@ classdef Household
         Q
 
         % Modeling 
-        is_first_house
-        is_bypass_house
         nx
         ny
         nu_mv
-
-        % Step sizes - Lagrange multipliers 
-        alfa_m_O_pred
-        alfa_m_O_succ
-        alfa_m_R_pred
-        alfa_m_R_alfa
-        alfa_T_F_pred
-        alfa_T_F_succ
-        alfa_T_R_pred
-        alfa_T_R_succ
-
-        % Simulink names 
-        nmpcBlockPathName
-        nmpcBusName
-        storageBusName
+        nu_md
 
         % Parameters struct and NMPC object for block in Simulink
-        params
         nlobj
-        Bus
     end
 
     
     methods
-        function obj = Household(is_first_house, is_bypass_house, T_set, T_amb, Ts, K, Q, nmpcBlockPathName, nmpcBusName, storageBusName)
-            
-            % Set the first_house and bypass_house properties
-            obj.is_first_house = is_first_house;
-            obj.is_bypass_house = is_bypass_house;
-
+        function obj = Household(T_set, T_amb, Ts, K, Q)
+ 
             % Set modeling dimensions properties 
-            if is_bypass_house
-                obj.nx = 7;
-            else
-                obj.nx = 6;
-            end
-
+            obj.nx = 4;
             obj.ny = 1;
-            obj.nu_mv = 7; 
+            obj.nu_mv = 1;
+            obj.nu_md = 1;
            
             % Set temperature values
             obj.T_amb = T_amb;
@@ -93,21 +58,13 @@ classdef Household
             obj.Ts = Ts;
             obj.Q = Q;
 
-            % Simulink names
-            obj.nmpcBlockPathName = nmpcBlockPathName;
-            obj.nmpcBusName = nmpcBusName;
-            obj.storageBusName = storageBusName;
-
-            % Create Bus object
-            % obj.Bus = obj.createBus();
-
             % Create NMPC object
             obj.nlobj = obj.createNMPC();
         end
         
         function nlobj = createNMPC(obj)
             % Create NMPC object
-            nlobj = nlmpc(obj.nx, obj.ny, 'MV', [1:obj.nu_mv]);
+            nlobj = nlmpc(obj.nx, obj.ny, 'MV', obj.nu_mv, 'MD', obj.nu_mv+obj.nu_md );
 
             % NMPC parameters
             nlobj.PredictionHorizon = obj.K; 
