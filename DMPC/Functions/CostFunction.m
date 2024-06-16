23a4b3fa53e3838d06c17349ce5dddd467f0d324
Jackmastro <giacomma@student.ethz.ch>
2024-06-16 15:16:45 +0200

    DMPC
    cost function little corrections

--- DMPC/Functions/CostFunction.m
+++ DMPC/Functions/CostFunction.m
@@ -17,25 +17,24 @@ function cost = CostFunction(x, u, ~, ~, household)
     m_O  = u(:,5);
     m_R_succ_I = u(:,6);
     m_R  = u(:,7);
-    
 
     if household.is_bypass_house
 
         % Measured Disturbances
-        m_O_pred_pred = u(:,8);
-        m_R_I_pred = u(:,9);
-        T_F_pred_pred = u(:,10);
-        T_R_I_pred = u(:,11);
+        m_O_pred_pred   = u(:,8);
+        m_R_I_pred      = u(:,9);
+        T_F_pred_pred   = u(:,10);
+        T_R_I_pred      = u(:,11);
 
         lambda_m_O_pred = u(:,12);
         lambda_m_R_pred = u(:,13);
         lambda_T_F_pred = u(:,14);
         lambda_T_R_pred = u(:,15);
         
-        delta_m_O_pred = u(:,16);
-        delta_m_R_pred = u(:,17);
-        delta_T_F_pred = u(:,18);
-        delta_T_R_pred = u(:,19);
+        delta_m_O_pred  = u(:,16);
+        delta_m_R_pred  = u(:,17);
+        delta_T_F_pred  = u(:,18);
+        delta_T_R_pred  = u(:,19);
 
         cost =     household.Q .* norm(T_b - household.T_set).^2 ...
              + lambda_m_O_pred' * (m_F - m_O_pred_pred)...
@@ -50,21 +49,20 @@ function cost = CostFunction(x, u, ~, ~, household)
     elseif household.is_first_house
 
         % Measured Disturbances
-        m_O_I_succ = u(:,8);
-        m_R_succ_succ = u(:,9);
-        T_F_I_succ = u(:,10);
-        T_R_succ_succ = u(:,11);
+        m_O_I_succ      = u(:,8);
+        m_R_succ_succ   = u(:,9);
+        T_F_I_succ      = u(:,10);
+        T_R_succ_succ   = u(:,11);
 
         lambda_m_O_succ = u(:,12);
         lambda_m_R_succ = u(:,13);
         lambda_T_F_succ = u(:,14);
         lambda_T_R_succ = u(:,15);
         
-        delta_m_O_succ = u(:,16);
-        delta_m_R_succ = u(:,17);
-        delta_T_F_succ = u(:,18);
-        delta_T_R_succ = u(:,19);
-    
+        delta_m_O_succ  = u(:,16);
+        delta_m_R_succ  = u(:,17);
+        delta_T_F_succ  = u(:,18);
+        delta_T_R_succ  = u(:,19);
 
         cost =     household.Q .* norm(T_b - household.T_set).^2 ...
              + lambda_m_O_succ' * (m_O - m_O_I_succ)...
@@ -79,14 +77,14 @@ function cost = CostFunction(x, u, ~, ~, household)
     else
         
         % Measured Disturbances
-        m_O_pred_pred = u(:,8);
-        m_O_I_succ = u(:,9);
-        m_R_succ_succ = u(:,10);
-        m_R_I_pred = u(:,11);
-        T_F_pred_pred = u(:,12);
-        T_F_I_succ = u(:,13);
-        T_R_succ_succ = u(:,14);
-        T_R_I_pred = u(:,15);
+        m_O_pred_pred   = u(:,8);
+        m_O_I_succ      = u(:,9);
+        m_R_succ_succ   = u(:,10);
+        m_R_I_pred      = u(:,11);
+        T_F_pred_pred   = u(:,12);
+        T_F_I_succ      = u(:,13);
+        T_R_succ_succ   = u(:,14);
+        T_R_I_pred      = u(:,15);
         
         lambda_m_O_pred = u(:,16);
         lambda_m_O_succ = u(:,17);
@@ -97,14 +95,14 @@ function cost = CostFunction(x, u, ~, ~, household)
         lambda_T_R_pred = u(:,22);
         lambda_T_R_succ = u(:,23);
         
-        delta_m_O_pred = u(:,24);
-        delta_m_O_succ = u(:,25);
-        delta_m_R_pred = u(:,26);
-        delta_m_R_succ = u(:,27);
-        delta_T_F_pred = u(:,28);
-        delta_T_F_succ = u(:,29);
-        delta_T_R_pred = u(:,30);
-        delta_T_R_succ = u(:,31);
+        delta_m_O_pred  = u(:,24);
+        delta_m_O_succ  = u(:,25);
+        delta_m_R_pred  = u(:,26);
+        delta_m_R_succ  = u(:,27);
+        delta_T_F_pred  = u(:,28);
+        delta_T_F_succ  = u(:,29);
+        delta_T_R_pred  = u(:,30);
+        delta_T_R_succ  = u(:,31);
 
         cost =     household.Q .* norm(T_b - household.T_set).^2 ...
              + lambda_m_O_pred' * (m_F - m_O_pred_pred)...
