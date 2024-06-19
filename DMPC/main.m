eec7135152fb9e367090b4b45a9d69bb5041c1cb
Jackmastro <giacomma@student.ethz.ch>
2024-06-19 11:23:08 +0200

    DMPC
    assign buses ready

--- DMPC/main.m
+++ DMPC/main.m
@@ -41,20 +41,20 @@ nmpc_path_C = strcat(houses_address_path, '/C/C_NMPC');
 C = Household(false, true, T_set, T_amb, Ts, K, Q, nmpc_path_C, true);
 
 %% Load and open Simulink
-
-% Define the folder containing .mat files
-bus_folder_name = 'LoadBus';
-
-% Get a list of all .mat files in the folder
-mat_files = dir(fullfile(bus_folder_name, '*.mat'));
-
-% Load each .mat file
-for k = 1:length(mat_files)
-    mat_file = fullfile(bus_folder_name, mat_files(k).name);
-    load(mat_file);
-end
-
-% Define and load the simulation model
-simulation_name = 'Simulation';
-% load_system(simulation_name);
-% open_system(simulation_name);
+% 
+% % Define the folder containing .mat files
+% bus_folder_name = 'LoadBus';
+% 
+% % Get a list of all .mat files in the folder
+% mat_files = dir(fullfile(bus_folder_name, '*.mat'));
+% 
+% % Load each .mat file
+% for k = 1:length(mat_files)
+%     mat_file = fullfile(bus_folder_name, mat_files(k).name);
+%     load(mat_file);
+% end
+% 
+% % Define and load the simulation model
+% simulation_name = 'Simulation';
+% % load_system(simulation_name);
+% % open_system(simulation_name);
