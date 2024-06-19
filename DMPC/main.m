a72df58b43626c3c64924300d113b1713bdf132d
Jackmastro <giacomma@student.ethz.ch>
2024-06-19 11:55:39 +0200

    DMPC
    simulation add params

--- DMPC/main.m
+++ DMPC/main.m
@@ -2,24 +2,6 @@ clc;
 clear;
 close all;
 
-% Get the full path of the currently running script
-if isdeployed
-    % If the code is deployed, use the built-in method
-    scriptFullPath = mfilename('fullpath');
-else
-    % If running in the MATLAB environment, use the editor API
-    scriptFullPath = matlab.desktop.editor.getActiveFilename;
-end
-
-% Extract the directory part of the path
-[scriptDir, ~, ~] = fileparts(scriptFullPath);
-
-% Change the current directory to the script's directory
-cd(scriptDir);
-
-% Display the current directory to confirm the change
-disp(['Current directory changed to: ', scriptDir]);
-
 %% Set Network Objects
 % Set temperatures
 T_set = 350;
@@ -41,7 +23,24 @@ nmpc_path_C = strcat(houses_address_path, '/C/C_NMPC');
 C = Household(false, true, T_set, T_amb, Ts, K, Q, nmpc_path_C, true);
 
 %% Load and open Simulink
+% Get the full path of the currently running script
+% if isdeployed
+%     % If the code is deployed, use the built-in method
+%     scriptFullPath = mfilename('fullpath');
+% else
+%     % If running in the MATLAB environment, use the editor API
+%     scriptFullPath = matlab.desktop.editor.getActiveFilename;
+% end
 % 
+% % Extract the directory part of the path
+% [scriptDir, ~, ~] = fileparts(scriptFullPath);
+% 
+% % Change the current directory to the script's directory
+% cd(scriptDir);
+% 
+% % Display the current directory to confirm the change
+% disp(['Current directory changed to: ', scriptDir]);
+
 % % Define the folder containing .mat files
 % bus_folder_name = 'LoadBus';
 % 
