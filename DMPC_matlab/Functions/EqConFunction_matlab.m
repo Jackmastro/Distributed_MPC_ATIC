b51cd05b23ea3c5e484537b797d5c4d1f25bb350
Jackmastro <giacomma@student.ethz.ch>
2024-06-21 12:06:18 +0200

    DMPC MATLAB
    debugging equality constraints, jacobians

--- DMPC_matlab/Functions/EqConFunction_matlab.m
+++ DMPC_matlab/Functions/EqConFunction_matlab.m
@@ -1,30 +1,31 @@
 function ceq = EqConFunction_matlab(x, u, ~, params)
 
     is_bypass_house = params(49);
-    
-    % States
-    T_B  = x(7);
 
     % Inputs
-    T_F_pred_I = u(1);
+    % T_F_pred_I = u(1);
+    T_R_succ_I = u(2);
     m_F        = u(3);
     m_U        = u(4);
     m_O        = u(5);
     m_R_succ_I = u(6);
     m_R        = u(7);
 
+    % Constraints
+    ceq = [
+        m_F - (m_U + m_O);
+        m_R - (m_U + m_R_succ_I)
+    ];
+    
     if is_bypass_house
+        % States
+        T_B  = x(7);
+
         ceq = [
-            m_F - (m_U + m_O);
-            m_R - (m_U + m_R_succ_I);
+            ceq;
             m_R_succ_I - m_O;
-            T_F_pred_I - T_B
-            ]; 
-    else
-        ceq = [
-            m_F - (m_U + m_O);
-            m_R - (m_U + m_R_succ_I)
-            ]; 
+            T_R_succ_I - T_B
+        ];
     end
 
 end
